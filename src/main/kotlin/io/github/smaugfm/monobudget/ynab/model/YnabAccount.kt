81a777a2b9b8137262f43bfaaa4470478d329329
Liver-23 <liver23@gmail.com>
2025-07-15 16:51:13 +0300

    Update Model

--- src/main/kotlin/io/github/smaugfm/monobudget/ynab/model/YnabAccount.kt
+++ src/main/kotlin/io/github/smaugfm/monobudget/ynab/model/YnabAccount.kt
@@ -1,5 +1,6 @@
 package io.github.smaugfm.monobudget.ynab.model
 
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
@@ -7,14 +8,19 @@ data class YnabAccount(
     val id: String,
     val name: String,
     val type: YnabAccountType,
-    val onBudget: Boolean,
+    @SerialName("on_budget") val onBudget: Boolean,
     val closed: Boolean,
     val note: String?,
     val balance: Long,
-    val clearedBalance: Long,
-    val unclearedBalance: Long,
-    val transferPayeeId: String,
-    val directImportLinked: Boolean,
-    val directImportInError: Boolean,
+    @SerialName("cleared_balance") val clearedBalance: Long,
+    @SerialName("uncleared_balance") val unclearedBalance: Long,
+    @SerialName("transfer_payee_id") val transferPayeeId: String,
+    @SerialName("direct_import_linked") val directImportLinked: Boolean,
+    @SerialName("direct_import_in_error") val directImportInError: Boolean,
+    @SerialName("last_reconciled_at") val lastReconciledAt: String? = null,
+    @SerialName("debt_original_balance") val debtOriginalBalance: Long? = null,
+    @SerialName("debt_interest_rates") val debtInterestRates: Map<String, Int>? = null,
+    @SerialName("debt_minimum_payments") val debtMinimumPayments: Map<String, Long>? = null,
+    @SerialName("debt_escrow_amounts") val debtEscrowAmounts: Map<String, Long>? = null,
     val deleted: Boolean,
 )
